11d6221697accab704cb26efd824dc407ee668f5
sayem <m.s_rksr@hotmail.com>
2023-08-09 12:50:21 +0100

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -4,20 +4,12 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+//*****************MUHAMMAD SAYEM*****************
+//*****************22830734***********************
+//*****************CSYM019************************
 
---
--- Database: `coursedb`
---
 
--- --------------------------------------------------------
 
---
--- Table structure for table `courses`
---
 
 CREATE TABLE `courses` (
   `id` int(11) NOT NULL,
